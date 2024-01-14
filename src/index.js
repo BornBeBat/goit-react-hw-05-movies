178d9d26e615bd61aab7c0259cb0cc12a368e849
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2024-01-14 19:46:13 +0200

    return strict mode

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { App } from 'components';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/goit-react-hw-05-movies">
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
