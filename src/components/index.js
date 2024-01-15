ba86e13a0ba1f95f6631671494656ab21db79ca1
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2024-01-15 16:52:08 +0200

    reexport

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export * from './App/App.jsx';
 export * from './Layout/Layout.js';
 export * from './Cast/Cast.js';
 export * from './Reviews/Reviews.js';
+export * from './SearchForm/SearchForm';
+export * from './ListItem/ListItem';
